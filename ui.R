5db24e20156efbaf7c8e8183b8c568790819a600
Pat Molloy <pat.molloy@outlook.com>
2016-05-28 20:24:11 +0100

    Update ui.R

--- ui.R
+++ ui.R
@@ -11,6 +11,7 @@ shinyUI(
     titlePanel("World Bank Population Indices 1960 - 2014 DataTable"),
    
       h3("This example shiny project uses data from the World Bank - country populations 1960 - 2014"),
+      h3("The hosted version of this needs a moment or two to load - please be patient, it does work!"),
       h4("It uses the R package DT http://rstudio.github.io/DT/ which provides an R interface to the JavaScript library http://datatables.net"),
       h4("There are 248 rows in the data - corresponding mainly to countries, though there are some aggegrates - e.g. world"),
       h4("Two dropdowns allow you to select by country name and country code"),
@@ -19,6 +20,7 @@ shinyUI(
       h4("Pretty much all the formatting and control is provided directly by the DT package, making it very easy to use"),
       h4("The next obvious step is to plot selected data, but time does not permit (nor skill)"),
       h4("The github repo is here https://github.com/patmolloy/SimpleShinyTable"),
+    
     
     # Create a new Row in the UI for selectInputs
     fluidRow(
